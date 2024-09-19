2bb50540e375fcd7a4caf3387372114974f36605
LightningThunder1 <134855228+LightningThunder1@users.noreply.github.com>
2024-09-19 22:40:31 +1000

    Update types.h

--- src/types.h
+++ src/types.h
@@ -181,11 +181,11 @@ enum Value : int {
   VALUE_MATE_IN_MAX_PLY  =  VALUE_MATE - MAX_PLY,
   VALUE_MATED_IN_MAX_PLY = -VALUE_MATE + MAX_PLY,
 
-  PawnValueMg   = 128,   PawnValueEg   = 213,
-  KnightValueMg = 781,   KnightValueEg = 854,
-  BishopValueMg = 825,   BishopValueEg = 915,
-  RookValueMg   = 1276,  RookValueEg   = 1380,
-  QueenValueMg  = 2538,  QueenValueEg  = 2682,
+  PawnValueMg   = 100,   PawnValueEg   = 100,
+  KnightValueMg = 300,   KnightValueEg = 300,
+  BishopValueMg = 300,   BishopValueEg = 300,
+  RookValueMg   = 500,  RookValueEg   = 500,
+  QueenValueMg  = 900,  QueenValueEg  = 900,
   Tempo = 0,
 
   MidgameLimit  = 15258, EndgameLimit  = 3915
